2b389b701281af3562bfd16b2e334a7b71d75bfc
irwanadinata <irwanadinata9@gmail.com>
2026-03-02 10:40:14 +0700

    pertemuan 1 javascript

---
+++ pertemuan1.js
@@ -0,0 +1,35 @@
+// Variable
+// Const untuk variabel yang tidak akan di ubah nilainya nanti (permanen)
+const kelas = 10;
+
+//Let untuk variabel yang bisa diubah nilainya nanti
+let nama = "zuan";
+
+//Cara penulisan variable
+const nama_lengkap = "Irwan Adinata";
+const namaLengkap = "Irwan Adinata";
+
+// Tipe Data
+// String, Number, Boolean (True atau False), Array, Object
+// 1. String
+const Teks = "Saya sedang belajar JS"
+
+// 2. Number
+const Umur = 20;
+
+// 3.Boolean (True atau False)
+const jawaban = 30 < 20;
+console.log(jawaban);
+
+// 4.Array
+const namaSiswa = ["Rayhan", "Rafie", "Farros"];
+
+// 5.Object
+const identitasSiswa = {
+    id : 1,
+    nama_lengkap : "Rayhan Lengkap",
+    username : "ray1200",
+    umur : 16
+};
+
+console.log(namaSiswa)
