4f4e5147fad072ed0673438adf65924ec69fef14
irwanadinata <irwanadinata9@gmail.com>
2026-03-04 10:13:50 +0700

    pertemuan 2

---
+++ pertemuan2.js
@@ -0,0 +1,40 @@
+// Operator Aritmatika
+let a = 10;
+let b = 3;
+
+console.log("Penjumlahan", a+b);
+console.log("Pengurangan", a-b);
+console.log("Perkalian", a*b);
+console.log("Pembagian", a/b);
+console.log("Modulus(sisa bagi)", a%b);
+console.log("Perpangkatan", a**b);
+
+let x = 10;
+
+// Operator Penugasan
+x = x + 3; 
+x += 3;
+console.log(x);
+
+// Operator Logika (Boolean)
+let logika1 = true;
+let logika2 = false;
+
+//AND
+console.log(logika1 && logika2);
+//OR
+console.log(logika1 || logika2);
+//NOT (!)
+console.log(!logika1)
+
+//Operator Perbandingan
+let z = 10;
+let v = 10;
+let c = "20";
+
+console.log("perbandingan ==",v == c);
+console.log("perbandingan ===",v === c);
+console.log("Lebih dari", z > v);
+console.log("Kurang dari", z < v);
+console.log("Lebih dari sama dengan", z >= v);
+console.log("Lebih dari sama dengan", z <= v);
